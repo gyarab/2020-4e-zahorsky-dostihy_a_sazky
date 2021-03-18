0de44c16d1f96d9a971b0c0b42d339ae4956fd47
ZahorskyOndrej <56340108+ZahorskyOndrej@users.noreply.github.com>
2021-03-18 10:23:42 +0100

    Add files via upload

---
+++ BettingOnHorsesLogic/Contracts/TurnResult.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BettingOnHorsesLogic.Contracts
+{
+    /// <summary>
+    /// Třída představující průběh hry.
+    /// </summary>
+    public class TurnResult
+    {
+        /// <summary>
+        /// Kontroluje, zda-li hra skončila.
+        /// </summary>
+        public bool GameOver { get; set; }
+        /// <summary>
+        /// Kontroluje, zda-li hráč prošel startem.
+        /// </summary>
+        public bool StartPassing { get; set; }
+    }
+}
