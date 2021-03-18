2dbd737fb07a0982ab510c2cdfbc65f57b023c29
ZahorskyOndrej <56340108+ZahorskyOndrej@users.noreply.github.com>
2021-03-18 10:23:15 +0100

    Add files via upload

---
+++ BettingOnHorses/CardInformation.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BettingOnHorses
+{
+    /// <summary>
+    /// Interaction logic for CardInformation.xaml
+    /// </summary>
+    public partial class CardInformation : Window
+    {
+        public CardInformation()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowCardInformation_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
